172ee9811fce9ecfaf4ad2f9cd89654445a5edcf
Rattana <116434835+RatZ6913@users.noreply.github.com>
2023-02-11 08:55:52 +0100

    Add files via upload

---
+++ Hôpital MVC/index.php
@@ -0,0 +1,5 @@
+<?php
+include_once __DIR__ . './src/includes/head.php';
+include_once __DIR__ . './src/includes/header.php';
+
+?>
---
+++ Hôpital MVC/src/includes/header.php
@@ -0,0 +1,8 @@
+<header>
+  <nav>
+    <ul>
+      <li><a href="./templates/add-patients.php">Ajouter un patient</a></li>
+      <li><a href="./templates/patients.class.php">Ajouter un rendez-vous</a></li>
+    </ul>
+  </nav>
+</header>
