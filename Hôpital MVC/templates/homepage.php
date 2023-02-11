1bdc52052032c3a6a2aa8efd33f99aefd07b8a1c
Pheak Rattana <rattana.pheak.rp@gmail.com>
2023-02-11 09:07:11 +0100

    Update HomePage 2
    Update HomePage 2

---
+++ Hôpital MVC/templates/homepage.php
@@ -0,0 +1,17 @@
+<?php
+$title = "Page d'accueil";
+include_once __DIR__ . './../src/includes/head.php';
+include_once __DIR__ . './../src/includes/header.php';
+?>
+
+<body>
+  <section>
+    <h1>Bienvenue sur la page d'accueil</h1>
+  </section>
+
+  <div>
+    <h2>Accéder à :</h2>
+    <a href="">Ajouter un patient</a>
+    <a href="">Ajouter un rendez-vous</a>
+  </div>
+</body>
