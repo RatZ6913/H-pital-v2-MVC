bfe0db417873556ba2967dbae7a39fe77a837a32
Pheak Rattana <rattana.pheak.rp@gmail.com>
2023-03-03 05:29:23 +0100

    update changes

--- Hôpital MVC/src/includes/header.php
+++ Hôpital MVC/src/includes/header.php
@@ -1,8 +1,6 @@
 <header>
   <nav>
     <ul>
-      <!-- <li><a href="./templates/add-patients.php">Ajouter un patient</a></li> -->
-      <!-- <li><a href="./templates/add-appointments.php">Ajouter un rendez-vous</a></li> -->
       <li><a href="./">Accueil</a></li>
     </ul>
   </nav>
